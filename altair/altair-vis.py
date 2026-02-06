0bd052493357b6da3c6075feae7689cb69d7b3c5
Karinne Aiello <164554084+karinneaiello@users.noreply.github.com>
2026-02-06 00:04:52 -0500

    Update altair-vis.py

--- altair/altair-vis.py
+++ altair/altair-vis.py
@@ -1,1 +1,19 @@
-python altair
+# !pip install altair
+
+import altair as alt
+import pandas as pd
+
+penglings = pd.read_csv("../penglings.csv")
+
+chart = (alt.Chart(penglings).mark_circle(opacity=0.8).encode(x=alt.X("flipper_length_mm",
+                                                                      scale=alt.Scale(domainMin=170),
+                                                                      title="Flipper Length (mm)"),
+                                                              y=alt.Y("body_mass_g",
+                                                                      scale=alt.Scale(domainMin=2500),
+                                                                      title="Body Mass (g)"),
+                                                              color=alt.Color("species",scale=alt.Scale(domain=["Adelie", "Chinstrap", "Gentoo"],
+                                                                                                        range=["orange", "purple", "blue"])),
+                                                              size="bill_length_mm")
+                                                            .properties(title="Penguin Size by Species"))
+
+chart
